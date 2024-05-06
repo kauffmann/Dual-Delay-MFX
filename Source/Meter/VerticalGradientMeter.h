f548863c4b4efbc39af965e38c560618c6230d19
Michael Kauffmann <kauffmann.michael@gmail.com>
2024-05-06 11:42:40 +0200

    Update VerticalGradientMeter.h

--- Source/Meter/VerticalGradientMeter.h
+++ Source/Meter/VerticalGradientMeter.h
@@ -21,43 +21,43 @@
 	public:
 		VerticalGradientMeter(std::function<float()>&& valueFunction) : valueSupplier(std::move(valueFunction)) 
 		{
-			startTimerHz(24);
-			grill = ImageCache::getFromMemory(BinaryData::MeterBG2_png, BinaryData::MeterBG2_pngSize);
+		    startTimerHz(24);
+		    grill = ImageCache::getFromMemory(BinaryData::MeterBG2_png, BinaryData::MeterBG2_pngSize);
 			
 		}
 		
 		void paint(Graphics& g) override
 	        {
-			auto bounds = getLocalBounds().toFloat().reduced(3.5f);
+		    auto bounds = getLocalBounds().toFloat().reduced(3.5f);
 
-                        g.setColour(colour_2);
-			g.fillRect(bounds);
-			g.setGradientFill(gradient);
-			const auto scaledY = jmap(valueSupplier(), -60.0f, 6.0f, 0.0f, static_cast<float>(getHeight())); 
+                    g.setColour(colour_2);
+	   	    g.fillRect(bounds);
+		    g.setGradientFill(gradient);
+		    const auto scaledY = jmap(valueSupplier(), -60.0f, 6.0f, 0.0f, static_cast<float>(getHeight())); 
             
-                        // Prevent drawing if no audio input goes into plugin
-                       if (scaledY > 0)
-                           g.fillRect(bounds.removeFromBottom(scaledY));  // remove part of bounds, and will paint the returned rectangle at removed area, bounds determent how much of the gradient is visible.
+                    // Prevent drawing if no audio input goes into plugin
+                    if (scaledY > 0)
+                        g.fillRect(bounds.removeFromBottom(scaledY));  // remove part of bounds, and will paint the returned rectangle at removed area, bounds determent how much of the gradient is visible.
 		}
 
 		void resized() override
 		{
-			// Why not put this in constructor. Because bounds will be all 0, not known in constructor.
+		    // Why not put this in constructor. Because bounds will be all 0, not known in constructor.
 
-			const auto bounds = getLocalBounds().toFloat();
-                        gradient = ColourGradient{ Colours::white, bounds.getBottomLeft(), Colours::red, bounds.getTopLeft(), false };
-                        gradient.addColour(0.7, Colours::white);
+		    const auto bounds = getLocalBounds().toFloat();
+                    gradient = ColourGradient{ Colours::white, bounds.getBottomLeft(), Colours::red, bounds.getTopLeft(), false };
+                    gradient.addColour(0.7, Colours::white);
             
 		}
 		
 		void paintOverChildren(::juce::Graphics& g) override
 		{
-			g.drawImage(grill, getLocalBounds().toFloat().reduced(3.0f));
+		    g.drawImage(grill, getLocalBounds().toFloat().reduced(3.0f));
 		}
 
 		void timerCallback() override
 		{
-			repaint();
+		    repaint();
 		}
 	private:
 		std::function<float()> valueSupplier;
