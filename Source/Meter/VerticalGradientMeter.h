116da1c3475182ad024780e7b16ef1896cd63e55
Michael <kauffmann.michael@gmail.com>
2024-05-13 11:30:07 +0200

    edit

--- Source/Meter/VerticalGradientMeter.h
+++ Source/Meter/VerticalGradientMeter.h
@@ -1,13 +1,13 @@
 /*
   =============================================================================
 
-    VerticalGradientMete.h
-    
-    Main Author: Created by Akash Murthy https://github.com/Thrifleganger
-    Author (customized to my project): Michael Kauffmann
+	VerticalGradientMete.h
+
+	Main Author: Created by Akash Murthy https://github.com/Thrifleganger
+	Author (customized to my project): Michael Kauffmann
 
 	A Meter(VerticalGradientMeter) represent 1 ch. So two instances are needed to form a 2 ch Meter. A Meter is a cild component in MFXGainPanel(client), where Bounds are set.
-	
+
 
  = =============================================================================
 */
@@ -16,55 +16,54 @@
 #include <JuceHeader.h>
 
 
-	class VerticalGradientMeter : public Component, public Timer
+class VerticalGradientMeter : public Component, public Timer
+{
+public:
+	VerticalGradientMeter(std::function<float()>&& valueFunction) : valueSupplier(std::move(valueFunction))
 	{
-	public:
-		VerticalGradientMeter(std::function<float()>&& valueFunction) : valueSupplier(std::move(valueFunction)) 
-		{
-			startTimerHz(24);
-			grill = ImageCache::getFromMemory(BinaryData::MeterBG2_png, BinaryData::MeterBG2_pngSize);
-			
-		}
-		
-		void paint(Graphics& g) override
-		{
-			auto bounds = getLocalBounds().toFloat().reduced(3.5f);
+		startTimerHz(24);
+		grill = ImageCache::getFromMemory(BinaryData::MeterBG2_png, BinaryData::MeterBG2_pngSize);
 
-            g.setColour(colour_2);
-			g.fillRect(bounds);
-			DBG("valueHeight" << static_cast<float>(getHeight()));
-			g.setGradientFill(gradient);
-			const auto scaledY = jmap(valueSupplier(), -100.0f, 6.0f, 0.0f, static_cast<float>(getHeight())); // was -60, but min in-valueSupplier is -100 
-			DBG("value" << scaledY);
-            // Prevent drawing if no audio input goes into plugin
-            if (scaledY > 0)
-            g.fillRect(bounds.removeFromBottom(scaledY));  // remove part of bounds, and will paint the returned rectangle at removed area, bounds determent how much of the gradient is visible.
-		}
+	}
 
-		void resized() override
-		{
-			// Why not put this in constructor. Because bounds will be all 0, not known in constructor.
+	void paint(Graphics& g) override
+	{
+		auto bounds = getLocalBounds().toFloat().reduced(3.5f);
 
-			const auto bounds = getLocalBounds().toFloat();
-            gradient = ColourGradient{ Colours::white, bounds.getBottomLeft(), Colours::red, bounds.getTopLeft(), false };
-            gradient.addColour(0.8, Colours::white);  // was 0.7
-            
-		}
-		
-		void paintOverChildren(::juce::Graphics& g) override
-		{
-			g.drawImage(grill, getLocalBounds().toFloat().reduced(3.0f));
-		}
+		g.setColour(colour_2);
+		g.fillRect(bounds);
+		g.setGradientFill(gradient);
+		const auto scaledY = jmap(valueSupplier(), -60.0f, 6.0f, 0.0f, static_cast<float>(getHeight()));
 
-		void timerCallback() override
-		{
-			repaint();
-		}
-	private:
-		std::function<float()> valueSupplier;
-		ColourGradient gradient{};
-		Image grill;
-        
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VerticalGradientMeter);
+		// Prevent drawing if no audio input goes into plugin
+		if (scaledY > 0)
+			g.fillRect(bounds.removeFromBottom(scaledY));  // remove part of bounds, and will paint the returned rectangle at removed area, bounds determent how much of the gradient is visible.
+	}
 
-    };
+	void resized() override
+	{
+		// Why not put this in constructor. Because bounds will be all 0, not known in constructor.
+
+		const auto bounds = getLocalBounds().toFloat();
+		gradient = ColourGradient{ Colours::white, bounds.getBottomLeft(), Colours::red, bounds.getTopLeft(), false };
+		gradient.addColour(0.7, Colours::white);
+
+	}
+
+	void paintOverChildren(::juce::Graphics& g) override
+	{
+		g.drawImage(grill, getLocalBounds().toFloat().reduced(3.0f));
+	}
+
+	void timerCallback() override
+	{
+		repaint();
+	}
+private:
+	std::function<float()> valueSupplier;
+	ColourGradient gradient{};
+	Image grill;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VerticalGradientMeter);
+
+};
